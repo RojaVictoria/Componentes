c798813b4e31254f1e04e5dc0bce130f34c14a06
Victoria <vic.rojas@gmail.com>
2021-08-16 23:09:29 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Componente></Componente>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Componente from './components/Componente.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Componente
   }
 }
 </script>
 
 <style>
-#app {
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: 700;
+  color: green;
+  background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
+
 </style>
---
+++ src/components/Componente.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(character, $index) in characters" :key="$index">
+        <img :src="character.src" class="image"/> {{character.nombre}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Componente',
+  data: () => ({
+    characters: [
+      { nombre: 'Rick Sanchez', src: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg' },
+      { nombre: 'Morty Smith', src: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg' },
+      { nombre: 'Summer Smith', src: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg' },
+      { nombre: 'Beth Smith', src: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg' },
+      { nombre: 'Jerry Smith', src: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg' },
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+li {
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+}
+
+.image {
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  padding: .5em;
+}
+
+</style>
